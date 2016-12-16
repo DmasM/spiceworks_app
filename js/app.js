264b927c59581ed9d89cd90f4a89da2da778e9b4
Dimas Maulana <dmaulana@repacorp.local>
2016-12-16 10:35:40 -0500

    get access token

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     console.log("IN");
+    var login = new SW.Login({appUid: "794c39f957150fded7e5e3d9b28ae657cbed44406c3a23aaf9f5815c32497532"});
+    
+    login.request('login').then(function(access_token){
+        console.log(access_token);
+    }, function(errors){
+        console.log(errors);
+    })
+
     var card = new SW.Card();
     var inventory = card.services('inventory');
     var environment = card.services('environment');
